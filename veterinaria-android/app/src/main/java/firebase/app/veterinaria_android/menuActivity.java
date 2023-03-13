feb8dbe4d39d22ca6671557ed57d0dbd306f8a1a
Erick Flores <xerickcua@gmail.com>
2023-03-13 17:54:16 -0500

    arreglando avance 1

--- veterinaria-android/app/src/main/java/firebase/app/veterinaria_android/menuActivity.java
+++ veterinaria-android/app/src/main/java/firebase/app/veterinaria_android/menuActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class menuActivity extends AppCompatActivity {
+
+    private BottomNavigationView btnNavigationMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        btnNavigationMenu = findViewById(R.id.btnNavigationMenu);
+        btnNavigationMenu.setItemIconTintList(null);
     }
 }

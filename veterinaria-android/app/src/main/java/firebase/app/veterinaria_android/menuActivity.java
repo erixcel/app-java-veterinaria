aaea7d652db2c0e5f730f63a70b7a8ac964d3d53
Erick Flores <xerickcua@gmail.com>
2023-03-27 16:33:24 -0500

    cambiando firebase

--- veterinaria-android/app/src/main/java/firebase/app/veterinaria_android/menuActivity.java
+++ veterinaria-android/app/src/main/java/firebase/app/veterinaria_android/menuActivity.java
@@ -1,6 +1,7 @@
 package firebase.app.veterinaria_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -17,5 +18,26 @@ public class menuActivity extends AppCompatActivity {
 
         btnNavigationMenu = findViewById(R.id.btnNavigationMenu);
         btnNavigationMenu.setItemIconTintList(null);
+        btnNavigationMenu.setSelectedItemId(R.id.interfaz_adoptar);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.frameMenu, new fragment_adoptar()).commit();
+
+        btnNavigationMenu.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.interfaz_citas:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameMenu, new fragment_citas()).commit();
+                    return true;
+                case R.id.interfaz_dar_adopcion:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameMenu, new fragment_adopcion()).commit();
+                    return true;
+                case R.id.interfaz_adoptar:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameMenu, new fragment_adoptar()).commit();
+                    return true;
+                case R.id.interfaz_pendientes:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameMenu, new fragment_pendientes()).commit();
+                    return true;
+            }
+            return false;
+        });
     }
 }

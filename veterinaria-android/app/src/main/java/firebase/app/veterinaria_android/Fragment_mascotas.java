edaeae68bae8ba76a26aaca52c0a0d42662eac29
Erick Flores <xerickcua@gmail.com>
2023-03-30 15:17:24 -0500

    crud 75%

--- veterinaria-android/app/src/main/java/firebase/app/veterinaria_android/Fragment_mascotas.java
+++ veterinaria-android/app/src/main/java/firebase/app/veterinaria_android/Fragment_mascotas.java
@@ -3,30 +3,66 @@ package firebase.app.veterinaria_android;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import firebase.app.veterinaria_android.adapter.MascotaAdapter;
+import firebase.app.veterinaria_android.modelo.Mascota;
 
 
 public class Fragment_mascotas extends Fragment {
 
     FloatingActionButton btnAgregarMascota;
+    RecyclerView recyclerMascotas;
+    MascotaAdapter mascotaAdapter;
+    FirebaseFirestore firestore;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mascotas, container, false);
+
+        // configuracion firestore
+        firestore = FirebaseFirestore.getInstance();
+        Query query = firestore.collection("mascotas");
+        FirestoreRecyclerOptions<Mascota> firestoreRecyclerOptions = new FirestoreRecyclerOptions.Builder<Mascota>().setQuery(query,Mascota.class).build();
+
+        // configuracion del adapter
+        mascotaAdapter = new MascotaAdapter(firestoreRecyclerOptions);
+        mascotaAdapter.notifyDataSetChanged();
+
+        // configuracion del recycleMascotas
+        recyclerMascotas = view.findViewById(R.id.recyclerMascotas);
+        recyclerMascotas.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerMascotas.setAdapter(mascotaAdapter);
+
+        // configuracion boton agregar
         btnAgregarMascota = view.findViewById(R.id.btnAgregarMascota);
-
-        // quitar fondo por defecto del boton
         btnAgregarMascota.setBackgroundTintMode(null);
-
-        // establecer metodo click
         btnAgregarMascota.setOnClickListener( click -> {
             Dialog_crearmascota modal = new Dialog_crearmascota();
             modal.show(getParentFragmentManager(),"Navegar");
         });
         return view;
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mascotaAdapter.startListening();
+    }
+
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        mascotaAdapter.stopListening();
     }
 }
--- veterinaria-android/app/src/main/java/firebase/app/veterinaria_android/adapter/MascotaAdapter.java
+++ veterinaria-android/app/src/main/java/firebase/app/veterinaria_android/adapter/MascotaAdapter.java
@@ -1,4 +1,48 @@
 package firebase.app.veterinaria_android.adapter;
 
-public class MascotaAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+import firebase.app.veterinaria_android.R;
+import firebase.app.veterinaria_android.modelo.Mascota;
+
+public class MascotaAdapter extends FirestoreRecyclerAdapter<Mascota, MascotaAdapter.ViewHolder>{
+
+    public MascotaAdapter(@NonNull FirestoreRecyclerOptions<Mascota> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull ViewHolder viewHolder, int position, @NonNull Mascota mascota) {
+        viewHolder.txtNombre.setText(mascota.getNombre());
+        viewHolder.txtSexo.setText(mascota.getSexo());
+        viewHolder.txtEdad.setText(mascota.getEdad());
+        viewHolder.txtRaza.setText(mascota.getRaza());
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_mascota, parent, false);
+        return new ViewHolder(view);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txtNombre, txtSexo, txtEdad, txtRaza;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtNombre = itemView.findViewById(R.id.txtNombre);
+            txtSexo = itemView.findViewById(R.id.txtSexo);
+            txtEdad = itemView.findViewById(R.id.txtEdad);
+            txtRaza = itemView.findViewById(R.id.txtRaza);
+        }
+    }
 }
